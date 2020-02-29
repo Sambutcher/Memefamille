c8d9a312d979bcdae5e3ec02a0441c3423788e45
Sambutcher <Samuel.boucher@gmail.com>
2020-02-29 16:03:37 +0100

    suppression du bazar

--- js/main.js
+++
@@ -1,9 +0,0 @@
-//Installation du service worker
-window.onload = () => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
-}
