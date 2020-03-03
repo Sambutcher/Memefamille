d0012695ddaa5adfee3e7515ee50491392e12793
Sambutcher <Samuel.boucher@gmail.com>
2020-03-03 08:26:22 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-//Installation du service worker
-window.onload = () => {
-  'use strict';
+var tabflechies;
+var tablemmes;
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
-}
+$( document ).ready(function() {
+    $('#page1').show();
+
+});
+
+$('#goracine').click(function(){
+  var mot=$('#montexte').val();
+  var i= tabflechies.indexOf(mot);
+  var j= tabflechies.lastIndexOf(mot);
+
+  var res="";
+  for (let n=i;n<=j;n++){
+    res=res+tablemmes[n]+"<BR>";
+
+    console.log(tablemmes[n]);
+  };
+
+  $('#result').html(tablemmes[i]+"<BR>"+tablemmes[j]);
+
+});
