833d05eb46b4e1f867f711941a460158e89fd21e
Sambutcher <Samuel.boucher@gmail.com>
2020-03-09 22:25:54 +0100

    test

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 
 $( document ).ready(function() {
     $('#page1').show();
+    import {table1,table2} from './js/tables.js';
 
 });
 
-import {table1,table2} from './js/tables.js';
+
 //const table1=["a","b","b","b","c","g"];
 //const table2=["d","e","a","c","b","a"];
 
